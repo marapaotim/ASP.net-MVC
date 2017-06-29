6c8ab199274f5674e4d594157e940699f25e69ab
Tim Marapao <marapaotim@gmail.com>
2017-06-28 19:43:36 -0700

    some message goes here

---
+++ WebApplication4/Controllers/EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee 
+       
+        public ActionResult Index()
+        {
+            MVCEntities db = new MVCEntities();
+            ViewBag.ListEmployee = db.employees.ToList(); 
+            return View();
+        }
+
+        public ActionResult NewEmployee()
+        {
+            return View();
+        }
+
+        public ActionResult InsertEmployee(EmployeeModel model)
+        {
+            try
+            {
+                MVCEntities db = new MVCEntities();
+                employee employ = new employee();
+                employ.Name = model.Name;
+                employ.Gender = model.Gender;
+                employ.Email = model.Email;
+                employ.Age = model.Age;
+                db.employees.Add(employ);
+                db.SaveChanges(); 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            MVCEntities db = new MVCEntities();
+            var data = db.employees.Where(x => x.id == id).First();
+            db.employees.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MVCEntities db = new MVCEntities();
+            var data = db.employees.Where(x => x.id == id).First();
+            employee employ = new employee();
+            employ.Name = data.Name.Trim();
+            employ.Gender = data.Gender.Trim();
+            employ.Email = data.Email.Trim();
+            employ.Age = data.Age.Trim(); 
+            return View(employ);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(employee model)
+        {
+            MVCEntities db = new MVCEntities();
+            if (ModelState.IsValid)
+            { 
+                var data = db.employees.Where(x => x.id == model.id).First();
+                data.Name = model.Name;
+                data.Gender = model.Gender;
+                data.Email = model.Email;
+                data.Age = model.Age;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Models/EmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int id;
+
+        [Required(ErrorMessage = "Name field is empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Gender field is empty")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Email field is empty")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Age field is empty")]
+        public string Age { get; set; }
+    }
+}
